5ffdd50cab7cf8703a8eba8a9da1084f026d7e89
Zach Gage <zach@gage.nyc>
2026-07-01 12:41:18 -0400

    Initial Commit

---
+++ gitchat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitchat
+//
+//  Created by Zach Gage on 7/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
